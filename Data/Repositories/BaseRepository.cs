ec861afd7659fd41321f59e731cf5d9a72ae5940
Uladzislau <vladislaugood@gmail.com>
2023-06-06 18:29:48 +0300

    change irepository add efcontext

--- Data/Repositories/BaseRepository.cs
+++ Data/Repositories/BaseRepository.cs
@@ -4,39 +4,46 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Data.Repositories
 {
-    public abstract class BaseRepository<T> : IRepository<T> 
-    where T: BaseModel
+    public abstract class BaseRepository<T> : IRepository<T>
+    where T : BaseModel
     {
         private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
 
         protected BaseRepository(DbContext context)
         {
             _context = context;
+            _dbSet = _context.Set<T>();
         }
 
-        public bool Add(T model)
+        public async Task<bool> SaveAsync(T model)
         {
-            throw new NotImplementedException();
-        }
-
-        public bool Change(T model)
-        {
-            throw new NotImplementedException();
+            if (model.Id > 0)
+            {
+                _dbSet.Update(model);
+            }
+            else
+            {
+                _dbSet.Add(model);
+            }
+            await _context.SaveChangesAsync();
+            return true;
         }
 
         public bool Delete(T model)
         {
-            throw new NotImplementedException();
+            _dbSet.Remove(model);
+            return true;
         }
 
-        public T GetAll()
+        public IList<T> GetAll()
         {
-            throw new NotImplementedException();
+            return _dbSet.ToList();
         }
 
         public T GetById(int Id)
         {
-            throw new NotImplementedException();
+            return _dbSet.SingleOrDefault(e => e.Id == Id);
         }
     }
 }
