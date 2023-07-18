1614d318d12917596caff8baf207d82f8c6bb157
Евгений Красовский <semmitwinsboss@gmail.com>
2023-07-18 15:25:54 +0300

    Add auth and reg pages and controlles and dtos and etc...

---
+++ WebApp/Dtos/Auth/AuthorizationDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Dtos.Auth
+{
+    public class Registration
+    {
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ WebApp/Dtos/Auth/RegistrationDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Dtos.Auth
+{
+    public class RegistrationDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
